5b9a385c10e8f68e0e45c493f202b3797b200264
FzzH Tech <149751791+Farhanzh123@users.noreply.github.com>
2026-05-28 23:58:12 +0700

    Update tugas5.dart

--- tugas5/tugas5/tugas5.dart
+++ tugas5/tugas5/tugas5.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  print("Masukkan nilai angka A : ");
+  print("Masukkan nilai angka A : iyey ");
   var A = int.parse(stdin.readLineSync()!);
 
   if (A % 2 == 0) {
